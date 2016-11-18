1bef9fe4220bdfcb700cae9fd436a83d13a27c02
calla95 <calla95@hotmail.it>
2016-11-18 22:45:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+ function aggiungiItem(){
+   document.getElementById("myP").style.display = "block";   //contrario "none"
+} 
+
+
+function annulla(){
+   document.getElementById("myP").style.display = "none";   //contrario "none"
+} 
+
+
+function aggiungiRiga(id_table){
+	var table = document.getElementById(id_table);
+	var tbody = table.getElementsByTagName('tbody')[0];
+	var colonne = table.getElementsByTagName('th').length;	
+	var tr = document.createElement('tr');
+    
+    var td = document.createElement('td');
+    var tx = document.createTextNode("nomeOggetto"));
+    td.appendChild(tx);
+    tr.appendChild(td);
+
+    var td = document.createElement('td');
+    var tx = document.createTextNode("quantita"));
+    td.appendChild(tx);
+    tr.appendChild(td);
+	   // for(var i=0; i<colonne; i++){
+		//var td = document.createElement('td');
+		//var tx = document.createTextNode(prompt("Inserisci testo per cella "+(i+1),""));
+		//td.appendChild(tx);
+		//tr.appendChild(td);
+	//} 
+	tbody.appendChild(tr);
+}
+
+
+
+    
